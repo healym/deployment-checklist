1e8952eab7a71cc65a0ad2567d44d7b93f71c7d9
Matthew Healy <mhealy@cboe.com>
2020-02-11 15:11:54 -0600

    Stub out factory for args

--- checklist.py
+++ checklist.py
@@ -1,32 +1,34 @@
 import argparse
-import datetime
+from datetime import datetime
 
 
 def checklist_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--config-file',
-                        dest='config_file',
-                        default='config.json',
-                        help='JSON file containing checklist')
-    parser.add_argument('-p', '--progress-file',
-                        dest='progress_file',
-                        default='checklist_{timestamp}.md'.format(timestamp=datetime.now().strftime('%Y_%m_%d')),
-                        help='Record of checklist responses')
-    parser.add_argument('-r', '--resume',
-                        dest='resume',
-                        default=False,
-                        action='store_true',
-                        help='Restore from previous run')
+
     return parser.parse_args()
 
 
 class Checklist:
 
-    def __init__(self, args):
-        '''
-        param args: argparse object containing expected command-line args
-        '''
-        pass
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.date = datetime.now()
+        self.date_str = self.date.strftime('%Y_%m_%d')
+
+    def _set_options(self):
+        self.parser.add_argument('-f', '--config-file',
+                                dest='config_file',
+                                default='config.json',
+                                help='JSON file containing checklist')
+        self.parser.add_argument('-p', '--progress-file',
+                                 dest='progress_file',
+                                 default=f'checklist_{self.date_str}.md'),
+                                 help='Record of checklist responses')
+        self.parser.add_argument('-r', '--resume',
+                                 dest='resume',
+                                 default=False,
+                                 action='store_true',
+                                 help='Restore from previous run')
 
     def run(self):
         pass
